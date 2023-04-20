79ee8cf5ee6d14e0158ea2d379acdbe5e49ce1ec
Plamen Hristov <14073311+plamenh@users.noreply.github.com>
2023-04-20 15:50:15 +0300

    Update account.rs print orders

--- src/futures/account.rs
+++ src/futures/account.rs
@@ -366,6 +366,7 @@ impl FuturesAccount {
             price_protect: None,
         };
         let order = self.build_order(sell);
+        println!("{:?}", order);
         let request = build_signed_request(order, self.recv_window)?;
         self.client
             .post_signed(API::Futures(Futures::Order), request)
@@ -394,6 +395,7 @@ impl FuturesAccount {
             price_protect: None,
         };
         let order = self.build_order(sell);
+        println!("{:?}", order);
         let request = build_signed_request(order, self.recv_window)?;
         self.client
             .post_signed(API::Futures(Futures::Order), request)
@@ -424,6 +426,7 @@ impl FuturesAccount {
             price_protect: None,
         };
         let order = self.build_order(sell);
+        println!("{:?}", order);
         let request = build_signed_request(order, self.recv_window)?;
         self.client
             .post_signed(API::Futures(Futures::Order), request)
@@ -454,6 +457,7 @@ impl FuturesAccount {
             price_protect: None,
         };
         let order = self.build_order(sell);
+        println!("{:?}", order);
         let request = build_signed_request(order, self.recv_window)?;
         self.client
             .post_signed(API::Futures(Futures::Order), request)
